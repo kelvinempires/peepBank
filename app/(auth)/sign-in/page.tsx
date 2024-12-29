4695443e9cbbecc3460e863f7f1f79937d010e82
kelvinempire <kelvinewurum@gmail.com>
2024-12-29 15:10:57 +0100

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,19 @@
+// import AuthForm from '@/components/AuthForm'
+
+// const SignIn = () => {
+//   return (
+//     <section className="flex-center size-full max-sm:px-6">
+//       <AuthForm type="sign-in" />
+//     </section>
+//   )
+// }
+
+// export default SignIn
+import React from 'react'
+
+export const SignIn = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,19 @@
+// import AuthForm from '@/components/AuthForm'
+
+// const SignUp = async () => {
+//   return (
+//     <section className="flex-center size-full max-sm:px-6">
+//       <AuthForm type="sign-up" />
+//     </section>
+//   )
+// }
+
+// export default SignUp
+import React from 'react'
+
+export const SignUp = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+export default SignUp
